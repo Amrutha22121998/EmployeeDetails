824b9ff166665d06d6d1fa198de1d9e06506330f
Amrutha KV <amrutha.kv@msf.ad>
2021-10-26 18:53:59 +0530

    new commit

--- src/main/java/com/Servlets/Delete.java
+++ src/main/java/com/Servlets/Delete.java
@@ -1,27 +1,13 @@
 package com.Servlets;
 
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.json.JSONException;
 import org.json.JSONObject;
 
-import com.data.Data;
 import com.dbconnection.JDBC;
 
-/**
- * Servlet implementation class create
- */
 @SuppressWarnings("serial")
-@WebServlet("/Delete")
 public class Delete extends MainServlet {
 
 	public Delete() {
@@ -29,36 +15,8 @@ public class Delete extends MainServlet {
 
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
-	// use proper naming convention for classes
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException, JSONException {
-		String payloadRequest = Data.getDataString(request);
-		JSONObject req = new JSONObject(payloadRequest);
-		JSONObject req1=new JSONObject();
-		req1.put("Request", req);
-		logger.info(req1);
-		JSONObject res = new JSONObject();
-		res = deleteEmployee(req);
-		 JSONObject req2=new JSONObject();
-		 req2.put("Response", res);
-	System.out.println(res);
-	logger.info(req2);
-		request.setCharacterEncoding("utf8");
-		response.setContentType("application/json");
-
-		PrintWriter out = response.getWriter();
-		try {
-			out.println(res);
-		} finally {
-			out.close();
-		}
-	}
-
-	private JSONObject deleteEmployee(JSONObject req) {
+	@Override
+	JSONObject Services(JSONObject req) {
 		JSONObject result = new JSONObject();
 		Connection connection = null;
 		PreparedStatement ps = null;
@@ -69,16 +27,13 @@ public class Delete extends MainServlet {
 
 			int reqId = req.getInt("Emp_code");
 			String SQLINSERT = "delete from  Employee where Emp_code = ? ";
-			System.out.println(SQLINSERT);
 			ps = connection.prepareStatement(SQLINSERT);
 			ps.setInt(1, reqId);
-			System.out.println(SQLINSERT);
 			int executeUpdate = ps.executeUpdate();
 			if (executeUpdate > 0) {
-					logger.info("Employee Details Deleted");
-					result.put("Message", "Deleted Succesfully");
-				}
-					 else {
+				logger.info("Employee Details Deleted");
+				result.put("Message", "Deleted Succesfully");
+			} else {
 				logger.info("Invalid id");
 				result.put("Message", "Invalid id");
 			}
@@ -94,5 +49,5 @@ public class Delete extends MainServlet {
 
 		return result;
 	}
-}
 
+}
--- src/main/java/com/Servlets/MainServlet.java
+++ src/main/java/com/Servlets/MainServlet.java
@@ -1,38 +1,61 @@
 package com.Servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
-import org.apache.log4j.PropertyConfigurator;
-import org.json.JSONException;
+import org.json.JSONObject;
 
-public class MainServlet extends HttpServlet {
+import com.data.Data;
+
+public abstract class MainServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
 	public static final Logger logger = Logger.getLogger(MainServlet.class);
 	
-    /**
-     * Default constructor. 
-     */
+   abstract JSONObject Services(JSONObject req);
     public MainServlet() {
     	super();
-        // TODO Auto-generated constructor stub
+       
     }
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-  		// TODO Auto-generated method stub
-  		//response.getWriter().append("Served at: ").append(request.getContextPath());
+  		
   	}
 	
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, JSONException {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String payloadRequest = Data.getDataString(request);
+		logger.info(payloadRequest);
 		
+	 JSONObject req = new JSONObject(payloadRequest);
+	 JSONObject req1=new JSONObject();
+	 req1.put("Request", req);
+		logger.info(req1);
+	
+		
+		JSONObject res=new JSONObject();
+		
+		res = Services(req);
+		
+		 JSONObject req2=new JSONObject();
+		 req2.put("Response", res);
+	System.out.println(res);
+	logger.info(req2);
+	
+		
+		request.setCharacterEncoding("utf8");
+        response.setContentType("application/json");
+		
+		 PrintWriter out = response.getWriter();
+	        try {
+	            out.println(res);
+	        } finally {
+	            out.close();
+	        }
 	}
 }
